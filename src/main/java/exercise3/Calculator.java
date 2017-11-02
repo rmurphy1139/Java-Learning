2ad54ccc90431cb53a638f42a10569af430298d9
Chris Mullaney <chris.mullaney@i-nexus.com>
2017-11-02 13:06:28 +0000

    Fix tests
    Used If/else if statements to cycle through the different math operations

--- src/main/java/exercise3/Calculator.java
+++ src/main/java/exercise3/Calculator.java
@@ -3,12 +3,34 @@ package exercise3;
 public class Calculator {
     /**
      * Performs a calculation using the provided operation on the two numbers
+     *
      * @param operationToPerform arithmetic operation to perform
-     * @param num1 first number
-     * @param num2 second number
+     * @param num1               first number
+     * @param num2               second number
      * @return result of the operation on the numbers
      */
     public double calculate(Operation operationToPerform, double num1, double num2) {
-        return 0.0;
+
+        double answer = 50.0;
+
+        if (operationToPerform == Operation.ADD) {
+
+            answer = num1 + num2;
+
+        } else if (operationToPerform == Operation.SUBTRACT) {
+
+            answer = num1 - num2;
+        } else if (operationToPerform == Operation.MULTIPLY) {
+
+            answer = num1 * num2;
+        } else if (operationToPerform == Operation.DIVIDE) {
+
+            answer = num1 / num2;
+        } else if (operationToPerform == Operation.MOD) {
+
+            answer = num1 % num2;
+        }
+
+        return answer;
     }
 }
