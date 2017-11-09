756a40a2c4a02777d90da270962b65953512164b
Chris Mullaney <chris.mullaney@i-nexus.com>
2017-11-09 13:15:18 +0000

    Test fixes & new test
    Fix tests for 15-0, 30-0 & 0-15.  New test 30-30 added

--- src/main/java/exercise4/Tennis.java
+++ src/main/java/exercise4/Tennis.java
@@ -4,6 +4,18 @@ import static exercise4.Score.*;
 
 public class Tennis {
     public GameScore calculateScore(GameScore currentGameScore, Player scoringPlayer) {
-        return new GameScore(LOVE, LOVE);
+
+        if (scoringPlayer == Player.Player1) {
+
+            if (currentGameScore.getPlayer1Score() == FIFTEEN) {
+                return new GameScore(THIRTY, LOVE);
+            } else {
+                return new GameScore(FIFTEEN, LOVE);
+            }
+        }
+        if (scoringPlayer == Player.Player2) {
+            return new GameScore(LOVE, FIFTEEN);
+        }
+        return null;
     }
 }
--- src/test/java/exercise4/TennisTest.java
+++ src/test/java/exercise4/TennisTest.java
@@ -1,5 +1,6 @@
 package exercise4;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static exercise4.Player.*;
@@ -8,13 +9,47 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class TennisTest {
+    Tennis tennis;
+    GameScore gameScore;
+
+    @Before
+    public void setup() {
+        tennis = new Tennis();
+    }
+
     @Test
     public void score_should_be_LOVE_FIFTEEN_when_player2_scores() {
-        final Tennis tennis = new Tennis();
-        final GameScore gameScore = new GameScore(LOVE, LOVE);
-        final GameScore updatedGameScore = tennis.calculateScore(gameScore, Player2);
+        gameScore = new GameScore(LOVE, LOVE);
+        GameScore updatedGameScore = tennis.calculateScore(gameScore, Player2);
 
         assertThat("Player 1 score is zero", updatedGameScore.getPlayer1Score(), is(LOVE));
         assertThat("Player 2 score is fifteen", updatedGameScore.getPlayer2Score(), is(FIFTEEN));
+    }
+
+    @Test
+    public void score_should_be_FIFTEEN_LOVE_when_player1_scores() {
+        gameScore = new GameScore(LOVE, LOVE);
+        GameScore updatedGameScore = tennis.calculateScore(gameScore, Player1);
+
+        assertThat("Player 1 score is fifteen", updatedGameScore.getPlayer1Score(), is(FIFTEEN));
+        assertThat("Player 2 score is zero", updatedGameScore.getPlayer2Score(), is(LOVE));
+    }
+
+    @Test
+    public void score_should_be_THIRTY_LOVE_when_player1_scores() {
+        gameScore = new GameScore(FIFTEEN, LOVE);
+        GameScore updatedGameScore = tennis.calculateScore(gameScore, Player1);
+
+        assertThat("Player 1 score is thirty", updatedGameScore.getPlayer1Score(), is(THIRTY));
+        assertThat("Player 2 score is zero", updatedGameScore.getPlayer2Score(), is(LOVE));
+    }
+
+    @Test
+    public void score_should_be_THIRTY_THIRTY_when_player1_scores() {
+        gameScore = new GameScore(FIFTEEN, THIRTY);
+        GameScore updatedGameScore = tennis.calculateScore(gameScore, Player1);
+
+        assertThat("Player 1 score is thirty", updatedGameScore.getPlayer1Score(), is(THIRTY));
+        assertThat("Player 2 score is thirty", updatedGameScore.getPlayer2Score(), is(THIRTY));
     }
 }
