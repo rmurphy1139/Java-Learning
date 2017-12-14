da966a53616d46fdf85d2952c22abba7e9311851
Chris Mullaney <chris.mullaney@i-nexus.com>
2017-12-14 12:34:26 +0000

    Add more tests

--- src/test/java/exercise4/GameStatusTest.java
+++ src/test/java/exercise4/GameStatusTest.java
@@ -58,4 +58,65 @@ public class GameStatusTest {
 
         assertThat("Game state should be " + expectedGameState, gameState, is(expectedGameState));
     }
+
+    @Test
+    public void game_should_be_in_progress_at_15_15() throws Exception {
+        setScoresAndAssert(FIFTEEN, FIFTEEN, IN_PROGRESS);
+    }
+
+    @Test
+    public void game_should_be_in_progress_at_15_30() throws Exception {
+        setScoresAndAssert(FIFTEEN, THIRTY, IN_PROGRESS);
+    }
+
+    @Test
+    public void game_should_be_in_progress_at_15_40() throws Exception {
+        setScoresAndAssert(FIFTEEN, FORTY, IN_PROGRESS);
+    }
+
+    @Test
+    public void game_should_be_in_progress_at_30_15() throws Exception {
+        setScoresAndAssert(THIRTY, FIFTEEN, IN_PROGRESS);
+    }
+
+    @Test
+    public void game_should_be_in_progress_at_30_30() throws Exception {
+        setScoresAndAssert(THIRTY, THIRTY, IN_PROGRESS);
+    }
+
+    @Test
+    public void game_should_be_in_progress_at_30_40() throws Exception {
+        setScoresAndAssert(THIRTY, FORTY, IN_PROGRESS);
+    }
+
+    @Test
+    public void game_should_be_in_progress_at_40_15() throws Exception {
+        setScoresAndAssert(FORTY, FIFTEEN, IN_PROGRESS);
+    }
+
+    @Test
+    public void game_should_be_in_progress_at_40_30() throws Exception {
+        setScoresAndAssert(FORTY, THIRTY, IN_PROGRESS);
+    }
+
+    @Test
+    public void game_should_be_won_by_player1_at_50_15() throws Exception {
+        setScoresAndAssert(FIFTY, FIFTEEN, WIN_P1);
+    }
+
+    @Test
+    public void game_should_be_won_by_player1_at_50_30() throws Exception {
+        setScoresAndAssert(FIFTY, THIRTY, WIN_P1);
+    }
+
+    @Test
+    public void game_should_be_won_by_player2_at_15_50() throws Exception {
+        setScoresAndAssert(FIFTEEN, FIFTY, WIN_P2);
+    }
+
+    @Test
+    public void game_should_be_won_by_player2_at_30_50() throws Exception {
+        setScoresAndAssert(THIRTY, FIFTY, WIN_P2);
+    }
+
 }
